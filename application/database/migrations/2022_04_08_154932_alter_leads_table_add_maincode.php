58996a48644755fa12da0fd37a58df75a034690a
Anurag Gupta1 <anurag.gupta1@ibm.com>
2022-04-09 17:00:44 +0530

    migrateion

--- application/database/migrations/2022_04_08_154932_alter_leads_table_add_maincode.php
+++ application/database/migrations/2022_04_08_154932_alter_leads_table_add_maincode.php
@@ -13,13 +13,15 @@ class AlterLeadsTableAddMaincode extends Migration
      */
     public function up()
     {
+        DB::table('leads')->truncate();
+
          Schema::table('leads', function (Blueprint $table) {
-            $table->string('main_code')->default(null)->unique();
-            $table->string('parent_code')->default(null);
+            $table->string('main_code');
+            $table->string('parent_code');
         });
         DB::statement('UPDATE `leads` SET `main_code`=null'); 
         DB::statement('ALTER TABLE `leads` ADD UNIQUE(`main_code`)'); 
-        DB::statement('ALTER TABLE `leads` ADD UNIQUE `unique_index`(`email`,`phone`)'); 
+        DB::statement('ALTER TABLE `leads` ADD UNIQUE `unique_index_emailphone`(`email`,`phone`)'); 
         DB::statement('ALTER TABLE `leads` CHANGE `parent_code` `parent_code` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL'); 
     }
 
