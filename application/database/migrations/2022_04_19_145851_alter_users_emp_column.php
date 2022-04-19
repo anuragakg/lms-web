436cf54d4fff52691d9d9b73d511987c728bb1d6
Anurag Gupta1 <anurag.gupta1@ibm.com>
2022-04-19 17:36:16 +0530

    added emp code

--- application/database/migrations/2022_04_19_145851_alter_users_emp_column.php
+++ application/database/migrations/2022_04_19_145851_alter_users_emp_column.php
@@ -13,16 +13,16 @@ class AlterUsersEmpColumn extends Migration
      */
     public function up()
     {
-        // Schema::table('users', function (Blueprint $table) {
-        //     $table->tinyinteger('emp_type')->default(1)->comment('1=users,2=employees');
-        //     $table->string('emp_code');
-        //     $table->string('dept');
-        //     $table->string('designation');
-        //     $table->string('rm');
-        //     $table->date('doj');
-        //     $table->string('phone');
-        // });
-        // DB::statement('UPDATE `users` SET `emp_type`=1'); 
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyinteger('emp_type')->default(1)->comment('1=users,2=employees');
+            $table->string('emp_code');
+            $table->string('dept');
+            $table->string('designation');
+            $table->string('rm');
+            $table->date('doj');
+            $table->string('phone');
+        });
+        DB::statement('UPDATE `users` SET `emp_type`=1'); 
     }
 
     /**
